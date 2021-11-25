924cc93951363198c752850c8cf2adb885a02616
Pedro Henrique <82953655+pedrodruviaro@users.noreply.github.com>
2021-11-25 20:07:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "./components/Button";
+
+export const App = () => {
+    return (
+        <>
+            <h1>React app</h1>
+            <h2>React app</h2>
+            <h3>React app</h3>
+            <h4>React app</h4>
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
+                nam iste necessitatibus ea, deserunt odio nesciunt non nostrum
+                porro vitae eaque qui, laudantium provident reiciendis libero,
+                tempore laboriosam iusto accusantium.
+            </p>
+
+            <Button>
+                <span>01</span>
+                button
+            </Button>
+        </>
+    );
+};
---
+++ src/styles.jsx/GlobalStyles.jsx
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+    body {
+        font-family: 'Spartan', sans-serif;
+    }
+
+
+    h1 {
+        font-size: 80px;
+        line-height: 103px;
+        font-family: 'Antonio', sans-serif;
+    }
+
+    h2 {
+        font-size: 40px;
+        line-height: 52px;
+        letter-spacing: -1.5;
+        font-family: 'Antonio', sans-serif;    
+    }
+
+    h3 {
+        font-size: 12px;
+        line-height: 25px;
+        letter-spacing: 2.6;
+        font-family: 'Spartan', sans-serif;
+    }
+
+    h3 {
+        font-size: 11px;
+        line-height: 25px;
+        letter-spacing: 1;
+        font-family: 'Spartan', sans-serif;
+    }
+
+    p, a, span {
+        font-size: 1rem;
+        line-height: 1.5rem;
+    }
+
+
+`;
