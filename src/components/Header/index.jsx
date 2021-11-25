38c912350587a0457f0cf8cd488d75ceea5b5b07
Pedro Henrique <82953655+pedrodruviaro@users.noreply.github.com>
2021-11-25 20:47:57 -0300

    add: Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,22 @@
 import React from "react";
-import { Container } from "./styles.jsx";
+import { Container, LinkNav } from "./styles.jsx";
+
+import planets from "../../data/data.json";
+
+const planetsNames = Object.keys(planets);
 
 export const Header = () => {
-    return <Container>haeder</Container>;
+    console.log(planetsNames);
+    return (
+        <Container>
+            <h1>The planets</h1>
+            <nav>
+                {planetsNames.map((name) => (
+                    <LinkNav color={planets[name].color} href="/" key={name}>
+                        {name}
+                    </LinkNav>
+                ))}
+            </nav>
+        </Container>
+    );
 };
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,3 +1,48 @@
 import styled from "styled-components";
 
-export const Container = styled.header``;
+export const Container = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.375rem 2rem;
+    height: 4rem;
+
+    h1 {
+        font-size: 1.75rem;
+        text-transform: uppercase;
+        font-weight: 400;
+        line-height: 1;
+    }
+
+    nav {
+        display: flex;
+        gap: 2rem;
+    }
+`;
+
+export const LinkNav = styled.a`
+    text-transform: uppercase;
+    font-size: 0.6875rem;
+    font-weight: 700;
+    letter-spacing: 1px;
+    display: block;
+    position: relative;
+    line-height: 4rem;
+
+    &::before {
+        content: "";
+        background-color: ${(props) => props.color};
+        width: 100%;
+        height: 3px;
+        display: block;
+        position: absolute;
+        top: 0;
+
+        transform: scale(0);
+        transition: transform 200ms;
+    }
+
+    &:hover::before {
+        transform: scale(1);
+    }
+`;
